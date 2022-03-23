f447dbafbec7fef1f9d16c2770f330e70f31fe10
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-23 16:38:56 -0300

    Update seq.py

--- seq.py
+++ seq.py
@@ -1,5 +1,3 @@
-# Escreva um código que faça o Blastn e Blastx de sequências (que serão inseridas em uma caixa de texto) usando o Streamlit e Biopython:
-
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -16,58 +14,33 @@ from Bio.Blast.Applications import NcbiblastnCommandline
 # Criando caixa de texto para inserir sequência
 st.title('Blastn e Blastx')
 st.write('Insira sua sequência:')
-seq = st.text_input('')
+seq = st.text_area('')
 
 # Criando botão para rodar o Blastn
 if st.button('Blastn'):
     st.write('Rodando Blastn...')
-    # Transformando sequência em um objeto SeqRecord
-    seq_obj = Seq(seq, IUPAC.unambiguous_dna)
-    seq_record = SeqRecord(seq_obj)
-    # Salvando o objeto em um arquivo FASTA
-    SeqIO.write(seq_record, 'seq.fasta', 'fasta')
-    # Rodando o Blastn
-    blastn_cline = NcbiblastnCommandline(query='seq.fasta', db='nt', evalue=0.001,
-                                         outfmt=5, out='blastn.xml')
-    stdout, stderr = blastn_cline()
-    # Abrindo o arquivo XML
-    blast_result_handle = open('blastn.xml')
-    blast_records = NCBIXML.parse(blast_result_handle)
-    # Lendo o arquivo XML
+    # Criando arquivo fasta
+    rec = SeqRecord(Seq(seq, IUPAC.unambiguous_dna), id='seq')
+    SeqIO.write(rec, 'seq.fasta', 'fasta')
+    # Rodando o blastn
+    result_handle = NCBIWWW.qblast("blastn", "nt", 'seq.fasta')
+    # Salvando o resultado
+    save_file = open("my_blast.xml", "w")
+    save_file.write(result_handle.read())
+    save_file.close()
+    result_handle.close()
+    # Lendo o resultado
+    result_handle = open("my_blast.xml")
+    blast_records = NCBIXML.parse(result_handle)
+    # Imprimindo o resultado
     for blast_record in blast_records:
         for alignment in blast_record.alignments:
             for hsp in alignment.hsps:
-                st.write(alignment.title)
-                st.write(hsp.expect)
+                st.write('****Alignment****')
+                st.write('sequence:', alignment.title)
+                st.write('length:', alignment.length)
+                st.write('e value:', hsp.expect)
                 st.write(hsp.query[0:75] + '...')
                 st.write(hsp.match[0:75] + '...')
-                st.write(hsp.sbjct[0:75] + '...')
-
-# Criando botão para rodar o Blastx
-if st.button('Blastx'):
-    st.write('Rodando Blastx...')
-    # Transformando sequência em um objeto SeqRecord
-    seq_obj = Seq(seq, IUPAC.unambiguous_dna)
-    seq_record = SeqRecord(seq_obj)
-    # Salvando o objeto em um arquivo FASTA
-    SeqIO.write(seq_record, 'seq.fasta', 'fasta')
-    # Rodando o Blastx
-    blastx_cline = NcbiblastnCommandline(query='seq.fasta', db='nr', evalue=0.001,
-                                         outfmt=5, out='blastx.xml')
-    stdout, stderr = blastx_cline()
-    # Abrindo o arquivo XML
-    blast_result_handle = open('blastx.xml')
-    blast_records = NCBIXML.parse(blast_result_handle)
-    # Lendo o arquivo XML
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
-            for hsp in alignment.hsps:
-                st.write(alignment.title)
-                st.write(hsp.expect)
-                st.write(hsp.query[0:75] + '...')
-                st.write(hsp.match[0:75] + '...')
-                st.write(hsp.sbjct[0:75] + '...')
-                
-# Criando botão para limpar a tela
-if st.button('Limpar'):
-    st.write('Limpeza concluída')
+                st.write(hsp.
+                         subject[0:75] + '...')
