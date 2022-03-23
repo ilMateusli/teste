077f4dca6b51a222923e1632fa4e2bc46c2827a5
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-23 20:32:53 -0300

    Update seq.py

--- seq.py
+++ seq.py
@@ -11,39 +11,31 @@ if text != "Insira sua sequência":
     st.balloons()
 
     blast = st.radio("Escolha o tipo de blast",("blastp","blastn","tblastn"))
-    input_fasta = """>seq1
-{}
-""".format(text)
+    input_fasta = Seq(text)
+    result_handle = NCBIWWW.qblast("blastn", "nr", input_fasta)
 
-    with open("fasta.fasta","w") as fasta:
-        fasta.write(input_fasta)
+    st.write("O blast está sendo realizado, aguarde")
+    st.balloons()
+    with open("my_blast.xml", "w") as out_handle:
+        out_handle.write(result_handle.read())
+    result_handle.close()
 
-    for seq_record in SeqIO.parse("fasta.fasta","fasta"):
-        print(seq_record.id)
-        print(repr(seq_record.seq))
-        print(len(seq_record))
-
-        result_handle = NCBIWWW.qblast(blast, "nr", seq_record.seq)
-        with open("my_blast.xml", "w") as out_handle:
-            out_handle.write(result_handle.read())
-        result_handle.close()
-
-        result_handle = open("my_blast.xml")
-        blast_records = NC
-        BIWWW.parse(result_handle)
-
-        E_VALUE_THRESH = 0.04
-        for blast_record in blast_records:
-            for alignment in blast_record.alignments:
-                for hsp in alignment.hsps:
-                    if hsp.expect < E_VALUE_THRESH:
-                        st.write("****Alignment****")
-                        st.write("sequence:", alignment.title)
-                        st.write("length:", alignment.length)
-                        st.write("e value:", hsp.expect)
-                        st.write(hsp.query[0:75] + "...")
-                        st.write(hsp.match[0:75] + "...")
-                        st.write(hsp.sbjct[0:75] + "...")
-                        st.balloons()
-                        st.balloons()
-        st.success("Sequência Analisada")
+    st.write("Salvando o resultado do blast, caso você queira abrir depois")
+    time.sleep(2)
+    st.balloons()
+    result_handle = open("my_blast.xml")
+    from Bio.Blast import NCBIXML
+    blast_record = NCBIXML.read(result_handle)
+    E_VALUE_THRESH = 0.01
+    for alignment in blast_record.alignments:
+        for hsp in alignment.hsps:
+            if hsp.expect < E_VALUE_THRESH:
+                st.write(alignment.title)
+                st.write(hsp.expect)
+                st.write(hsp.score)
+                st.write(hsp.query)
+                st.write(hsp.match)
+                st.write(hsp.sbjct)
+                time.sleep(2)
+    result_handle.close()
+    st.write("O arquivo foi salvo e pode ser baixado em my_blast.xml")
