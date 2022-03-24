40a1df5076b14675d172495ec80b593f751a29f8
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-24 18:38:59 -0300

    Update seq.py

--- seq.py
+++ seq.py
@@ -20,19 +20,4 @@ if text != "Insira sua sequência":
     save_file.close()
     result_handle.close()
     st.write("Blast finalizado, por favor aguarde enquanto o resultado é lido")
-    result_handle = open("blast.xml")
-    blast_records = NCBIXML.parse(result_handle)
-    blast_record = next(blast_records)
-    E_VALUE_THRESH = 0.04
-# Use o pandas para fazer um dataframe com os resultados e o st.table para mostrar o resultado
-    data = pd.DataFrame(columns=['sequence','length', 'e value'])
-    index = 0
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            if (hsp.expect < E_VALUE_THRESH):
-                data.loc[index] = [alignment.title, alignment.length, hsp.expect]
-                index+=1
-    st.table(data)
-    
-# Fazer um dataframe das regiões codificadoras e utilizar o plotly para mostrar os gráficos
-    st.plotly_chart(data)
+    st.write(result_handle)
