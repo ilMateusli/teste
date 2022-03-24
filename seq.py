3d07acb5a202eb6aef86e20447ca18e0848ee4c9
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-23 22:40:18 -0300

    Update seq.py

--- seq.py
+++ seq.py
@@ -13,30 +13,32 @@ if text != "Insira sua sequência":
 
     blast = st.radio("Escolha o tipo de blast",("blastp","blastn","tblastn"))
     input_fasta = Seq(text)
-    result_handle = NCBIWWW.qblast("blastn", "nr", input_fasta, format_type="XML")
+    result_handle = NCBIWWW.qblast("blastn", "nr", input_fasta)
 
     st.write("O blast está sendo realizado, aguarde")
     st.balloons()
-    with open("my_blast.xml", "w") as out_handle:
+# O repositório é do GitHub e por isso, os arquivos são necessários para rodar
+    with open("blast.xml", "w") as out_handle:
         out_handle.write(result_handle.read())
     result_handle.close()
 
-    st.write("Salvando o resultado do blast, caso você queira abrir depois")
+    st.write("Carregando resultados")
     st.balloons()
-    result_handle = open("my_blast.xml")
+
     from Bio.Blast import NCBIXML
-    blast_record = NCBIXML.read(result_handle)
+    result_handle = open("blast.xml")
+    blast_records = NCBIXML.parse(result_handle)
     E_VALUE_THRESH = 0.01
-    for alignment in blast_record.alignments:
-        for hsp in alignment.hsps:
-            if hsp.expect < E_VALUE_THRESH:
-                st.write(alignment.title)
-                st.write(hsp.expect)
-                st.write(hsp.score)
-                st.write(hsp.query)
-                st.write(hsp.match)
-                st.write(hsp.sbjct)
-                time.sleep(2)
-    result_handle.close()
-    st.write("O arquivo foi salvo e pode ser baixado em my_blast.xml")
-    st.write(result_handle)
+    for blast_record in blast_records:
+        for alignment in blast_record.alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect < E_VALUE_THRESH:
+                    st.write("****Alignment****")
+                    st.write("Sequence:", alignment.title)
+                    st.write("Length:", alignment.length)
+                    st.write("e value:", hsp.expect)
+                    st.write(hsp.query[0:75] + "...")
+                    st.write(hsp.match[0:75] + "...")
+                    st.write(hsp.sbjct[0:75] + "...")
+else:
+    st.write("Insira a sequência acima")
