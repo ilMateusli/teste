b5f93645388f07538971884a72e086a840adc061
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-03-24 18:27:10 -0300

    Update seq.py

--- seq.py
+++ seq.py
@@ -8,18 +8,13 @@ import numpy as np
 
 st.title("Labinftec Blastn")
 
-st.write("""
-    # Analisador de sequências
-    Este analisador utiliza o sistema de blast para analisar uma sequência de DNA e retornar o resultado do blast.
+text = st.text_area("Insira sua sequência")
 
-    **Para utilizar o analisador, basta inserir uma sequência e clicar no botão.**
-    """)
-
-st.write("Insira uma sequência de DNA para ser analisada:")
-input_fasta = st.text_area("", "CAACCGCTATTCCTCTTTTTGCAGGGGTTTTTCAAAATTATCAAGTTCCTCTTTTATCAGTATATGTTCAAGCTGCAAATTTACATTTATCAGTTTTGAGAGATGTTTCAGTGTTTGGACAAAGAAGACCTTTTAATATAGGGATAAATAATCAACAACGGCCTAGCCTCCCAGGTTTATCTGTTCTTGACGGGACAGAATTTGCTTATGGGACCTCCTCAAATTTGCCATCCGCTGTATACAGAAAAAGCGGAACGGTAGATTCGCTGGAT")
-
-if st.button("Analisar sequência"):
-    result_handle = NCBIWWW.qblast("blastn", "nr", input_fasta.format("fasta"))
+if text != "Insira sua sequência":
+    st.write("Analisando a sequência")    
+    blast = st.radio("Escolha o tipo de blast",("blastp","blastn","tblastn"))
+    input_fasta = text
+    result_handle = NCBIWWW.qblast(blast, "nr", input_fasta.format("fasta"))
     save_file = open("blast.xml", "w")
     save_file.write(result_handle.read())
     save_file.close()
@@ -29,16 +24,15 @@ if st.button("Analisar sequência"):
     blast_records = NCBIXML.parse(result_handle)
     blast_record = next(blast_records)
     E_VALUE_THRESH = 0.04
+# Use o pandas para fazer um dataframe com os resultados e o st.table para mostrar o resultado
+    data = pd.DataFrame(columns=['sequence','length', 'e value'])
+    index = 0
     for alignment in blast_record.alignments:
         for hsp in alignment.hsps:
-            if hsp.expect < E_VALUE_THRESH:
-                data = {'sequence': alignment.title, 'length': alignment.length, 'e value': hsp.expect, 'sbjct': hsp.sbjct[0:75] + "..."}
-                df = pd.DataFrame(data, columns = ['sequence', 'length', 'e value', 'sbjct'])
-                st.write("****Alignment****")
-                st.dataframe(df)
-
-st.write("""
-    ### Referências:
-    * https://www.ncbi.nlm.nih.gov/books/NBK279671/
-    * https://streamlit.io/docs/
-""")
+            if (hsp.expect < E_VALUE_THRESH):
+                data.loc[index] = [alignment.title, alignment.length, hsp.expect]
+                index+=1
+    st.table(data)
+    
+# Fazer um dataframe das regiões codificadoras e utilizar o plotly para mostrar os gráficos
+    st.plotly_chart(data)
